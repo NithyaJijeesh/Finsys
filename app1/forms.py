73a1117883f783110b4c138aefceacc8205b0f6d
Nithya Vijayan <niyavijayan@gmail.com>
2023-05-26 10:21:05 +0530

    first commit

---
+++ app1/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from .models import *
+from .models import noninventory
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = service
+        fields = ['img', 'name', 'sku', 'sac', 'unit','categ','descr','check1','saleprice','income','check2','tax','abatement','sertype']
+
+
+class ImageForm2(forms.ModelForm):
+    class Meta:
+        model=noninventory
+        fields=("image","name","sku","hsn","unit","category","initialqty","date","stockalrt","invacnt","description","salesprice","incomeacnt","tax","purchaseinfo","cost","expacnt","purtax","revcharge","presupplier",)
+
+
+class ImageForm3(forms.ModelForm):
+    class Meta:
+        model = bundle
+        fields = ['image', 'name', 'sku', 'description']
+
+class ImageForm4(forms.ModelForm):
+    class Meta:
+        model=inventory
+        fields=("image","name","sku","hsn","unit","category","initialqty","date","stockalrt","invacnt","description","salesprice","incomeacnt","tax","purchaseinfo","cost","expacnt","purtax","revcharge","presupplier")
+
+
+#----------sumayya------------------------------------------------------------------------------------------------------------
+class EmailForm(forms.Form):
+    email = forms.EmailField()
+    subject = forms.CharField(max_length=100)
+    attach = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    message = forms.CharField(widget = forms.Textarea )
